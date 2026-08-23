994e6b5458a35699d7d17907b8af7b34c114422a
TanThong435 <48750837+TanThong435@users.noreply.github.com>
2026-08-24 01:55:32 +0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit =0;
+        int left =0;
+        int right =1;
+
+        while (right < prices.length){
+            if (prices[left] > prices[right]){
+                left= right;
+            } else {
+                int profit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit, profit);
+
+            }
+            right++;
+
+        }
+    
+        return maxProfit;
+    }
+}
